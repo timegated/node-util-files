37f4acbc4a8d6d127cc6ab24ff0880b443dc4328
Daniel Beccaria <beccdaniel@gmail.com>
2021-11-11 20:32:37 -0800

    Created: links from a simple csv file

---
+++ createURLS.mjs
@@ -0,0 +1,26 @@
+import parse from 'csv-parse';
+import fs from 'fs';
+
+const csvData = [];
+
+const createUrls = (path) => {
+  fs.createReadStream(path)
+    .pipe(parse({ delimiter: ":" }))
+    .on('data', (csvrow) => {
+      const link = `https://gsi.learnbyloci.com/${csvrow[0].replace(/\s/g, '').toLowerCase()}`
+      csvData.push(link);
+    })
+    .on('end', () => {
+      console.log('Finished');
+      console.log(csvData);
+      fs.writeFile('school-urls.csv', csvData, 'utf8', function (err) {
+        if (err) {
+          console.log('Some error occured - file either not saved or corrupted file saved.');
+        } else{
+          console.log('It\'s saved!');
+        }
+      });
+    });
+};
+
+createUrls('./caschools.csv');
